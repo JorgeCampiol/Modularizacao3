ca880bdab3eeea31482164c93d97493d9e8be539
JorgeCampiol <jorgecampiol18@gmail.com>
2024-11-12 10:31:19 -0300

    Modularização3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
-public class Main {
+import java.util.Scanner;
+
+class Modularizacao3 {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        int numero = lerNumero(scanner);
+        boolean isPrimo = verificarPrimo(numero);
+        mostrarResultado(numero, isPrimo);
+
+        scanner.close();
+    }
+
+    private static int lerNumero(Scanner scanner) {
+        System.out.print("Digite um número: ");
+        return scanner.nextInt();
+    }
+
+    private static boolean verificarPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(numero); i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void mostrarResultado(int numero, boolean isPrimo) {
+        if (isPrimo) {
+            System.out.println(numero + " é primo.");
+        } else {
+            System.out.println(numero + " não é primo.");
+        }
     }
 }
